37eaa77aa6ebf5f1085244ba91db62c8d2bf6f28
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-02-23 11:24:46 -0300

    devCommit

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'desposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-02-21 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2022-02-22 09:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
