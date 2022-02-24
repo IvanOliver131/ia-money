04c2210ceacb8100852383f41add8120202f3a65
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-02-24 11:14:48 -0300

    finis ✨🚀🔥

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,7 +19,7 @@ export function Home() {
       await signInWithGoogle();
     }
 
-    const roomRef = await database.ref('dashboards'); 
+    const roomRef = database.ref('dashboards'); 
     let keyId;
 
     await roomRef.once('value', function(snapshot) {
