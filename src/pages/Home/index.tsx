89775a9da312e1acf5c2a1fa0af303128375e2e1
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-02-24 11:27:43 -0300

    devCommit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,34 +18,31 @@ export function Home() {
       await signInWithGoogle();
     } 
 
-    if (user) {
-      const roomRef = database.ref('dashboards'); 
-      let keyId;
+    const roomRef = database.ref('dashboards'); 
+    let keyId;
 
-      await roomRef.once('value', function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          let key = childSnapshot.key;
-          let data = childSnapshot.val();
+    await roomRef.once('value', function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        let key = childSnapshot.key;
+        let data = childSnapshot.val();
 
-          if (data.authorId.includes(user.id) === true) {
-            keyId = key;
-          } 
-        });
+        if (data.authorId.includes(user?.id) === true) {
+          keyId = key;
+        } 
       });
-    
-      if (keyId) {
-        navigate(`dashboard/${keyId}`);
-        
-        return;
-      } else {
-        const firebaseDash = await roomRef.push({
-          authorId: user.id
-        })
-        navigate(`/dashboard/${firebaseDash.key}`);
-      }
-    } else {
+    });
+    console.log(keyId)
+    if (keyId !== undefined) {
+      navigate(`dashboard/${keyId}`);
+
       return;
-    }
+    } 
+    
+    const firebaseDash = await roomRef.push({
+      authorId: user?.id
+    })
+    
+    navigate(`/dashboard/${firebaseDash.key}`);  
   }
   
   return (
